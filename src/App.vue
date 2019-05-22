37461f6655188cb7bbf9b38fa3a6c725960ee54e
gigahertz <jackgoodchen2019@gmail.com>
2019-05-22 16:56:29 +0800

    upd

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
   </div>
 </template>
 
+<script>
+  var view_width = document.getElementsByTagName('html')[0].getBoundingClientRect().width;
+  var _html = document.getElementsByTagName('html')[0];
+  view_width>640?_html.style.fontSize=640/16 +'px':_html.style.fontSize =view_width/16+'px';
+</script>
+
 <style>
 body {
   font-size: 16px;
